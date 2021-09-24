44579656c7e7fd6b65b4276f62324738aa6c589a
LucaLink <LucaLink@0-209-R14>
2021-09-24 09:06:24 +0200

    -

--- JavaWillNurSpielen/src/Vertex.java
+++ JavaWillNurSpielen/src/Vertex.java
@@ -1,9 +1,16 @@
 
 public class Vertex {
+	public double x;
+	public double y;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Vertex(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String toString() {
+		
+		return "x = " +x+", y = "+y;
 	}
 
 }
