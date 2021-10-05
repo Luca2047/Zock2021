2006ffd57c34af0f2e258fb20055f1921b84294a
LucaLink <LucaLink@0-208-13>
2021-10-05 14:53:08 +0200

    -

--- JavaWillNurSpielen/src/GeometricObject.java
+++ JavaWillNurSpielen/src/GeometricObject.java
@@ -1,4 +1,26 @@
 
 public class GeometricObject {
 
+	public Vertex pos;
+	public double width;
+	public double height;
+
+	public GeometricObject(double width, double height, Vertex pos) {
+		this.width = width;
+		this.height = height;
+		this.pos = pos;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public Vertex getPos() {
+		return pos;
+	}
+
 }
